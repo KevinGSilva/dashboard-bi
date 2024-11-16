7527bc4c80627e4b374d73a636b0fe25f96984ab
Kevin Silva <kevincaprino33@gmail.com>
2024-11-16 17:12:06 -0300

    Upload dos dados

---
+++ app/Models/Music.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Music extends Model
+{
+    use HasFactory;
+
+    protected $table = 'musics';
+
+    protected $fillable = [
+        'track',
+        'artist',
+        'album',
+        'year',
+        'duration',
+        'time_signature',
+        'danceability',
+        'energy',
+        'key',
+        'loudness',
+        'mode',
+        'speechiness',
+        'acousticness',
+        'instrumentalness',
+        'liveness',
+        'valence',
+        'tempo',
+        'popularity',
+    ];
+}
