3df943a7ad9794ac8f2b36f4f16963ef0fe5246e
Kevin Silva <kevincaprino33@gmail.com>
2024-11-17 22:34:13 -0300

    Layout principal

---
+++ resources/views/layouts/common/public.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    @include('layouts.includes.css')
+</head>
+<body>
+    <div class="container-fluid">
+        @yield('content')
+
+        <div class="spinner-background d-none">
+            <div class="sk-chase">
+                <div class="sk-chase-dot"></div>
+                <div class="sk-chase-dot"></div>
+                <div class="sk-chase-dot"></div>
+                <div class="sk-chase-dot"></div>
+                <div class="sk-chase-dot"></div>
+                <div class="sk-chase-dot"></div>
+            </div>
+        </div>
+    </div>
+
+    @include('layouts.includes.scripts')
+    @yield('scripts')
+</body>
+</html>
