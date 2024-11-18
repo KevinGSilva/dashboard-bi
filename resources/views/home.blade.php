0c47b91f149b7719186f8f6ea91f79e16a933c96
Kevin Silva <kevincaprino33@gmail.com>
2024-11-18 04:49:53 -0300

    separando graficos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -117,7 +117,7 @@
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <div class="col-lg-6 mb-2">
+                    <div class="col-lg-12 mb-2">
                         <div class="col-sm-12 col-lg-8">
                             <div class="col-lg-12 d-flex justify-content-between">
                                 <div class="col-lg-12">
@@ -129,7 +129,15 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-6 mb-2">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-12 mb-2">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-12 mb-2">
                         <div class="col-sm-12 col-lg-9">
                             <div class="col-lg-12 d-flex justify-content-between">
                                 <div class="col-lg-8">
